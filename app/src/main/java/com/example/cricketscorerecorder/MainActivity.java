7159e9e38fdcf3576a452cbb41d226c1f4d9b3b5
= <=>
2020-05-26 16:14:05 +0530

    Did some editing

--- app/src/main/java/com/example/cricketscorerecorder/MainActivity.java
+++ app/src/main/java/com/example/cricketscorerecorder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cricketscorerecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +10,13 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button one, two, three, four, six, wide, no, out;
+    Button convert;
+    EditText resultconvert;
     int runs = 0, wickets=0, balls=0;
+    double overs=0.0;
     EditText run, wicket, over;
+    Button Extra;
+    EditText exruns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +32,19 @@ public class MainActivity extends AppCompatActivity {
         wide = (Button)findViewById(R.id.wide);
         no = (Button)findViewById(R.id.no);
         out = (Button)findViewById(R.id.button2);
+        convert = (Button)findViewById(R.id.convert);
+        resultconvert = (EditText)findViewById(R.id.covertedresult);
+        Extra = (Button)findViewById(R.id.button3);
+        exruns = (EditText)findViewById(R.id.extra);
         one.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                runs = runs+1;
-                run.setText(Integer.toString(runs));
-                balls=balls+1;
-                over.setText(Integer.toString(balls));
-            }
-        });
+        public void onClick(View v) {
+            runs = runs+1;
+            run.setText(Integer.toString(runs));
+            balls=balls+1;
+            over.setText(Integer.toString(balls));
+        }
+    });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 runs = runs+3;
                 run.setText(Integer.toString(runs));
                 balls = balls+1;
-                over.setText(Integer.toString(balls));            }
+                over.setText(Integer.toString(balls));
+            }
         });
         four.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,16 +91,44 @@ public class MainActivity extends AppCompatActivity {
         no.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 runs = runs+1;
                 run.setText(Integer.toString(runs));
+
+
+
             }
         });
         out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wicket.setText(Integer.toString(wickets+1));
+                wickets = wickets+1;
+                wicket.setText(Integer.toString(wickets));
                 balls = balls+1;
                 over.setText(Integer.toString(balls));
+            }
+        });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String no=over.getText().toString();
+                int a = Integer.parseInt(no);
+                int rem = a%6;
+                int quotient = a/6;
+                resultconvert.setText((quotient+"."+rem));
+            }
+        });
+
+        Extra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String no = exruns.getText().toString();
+                int zero =0;
+                int a = Integer.parseInt(no);
+                runs = runs+a;
+                run.setText(Integer.toString(runs));
+                exruns.setText(Integer.toString(zero));
             }
         });
     }
